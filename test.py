c58d5668d8821bdcdd5f70b4293a1683535544a8
dpedrosac <david.pedrosa@staff.uni-marburg.de>
2020-08-31 10:04:27 +0200

    changes in test.py to sketch the first steps needed

--- test.py
+++ test.py
@@ -1,4 +1,27 @@
-for k in range(8):
-    print(k)
+import os
+import ants
+import glob
+import subprocess
 
-y = 12
+# Start with general commands
+if os.getlogin() == 'david':
+    rootdir = os.getcwd()
+    test_folder = os.path.join(rootdir, 'data/patients/', '4701P') # this is going to be the subject used to test all the scripts
+
+t2image = glob.glob(os.path.join(test_folder, '*t2*.nii.gz'))
+
+# Import files into ants
+test_data = ants.image_read(t2image[0])
+
+# Display images in itksnap, if installed
+cmd = ["itksnap", "-g", t2image[0]]
+
+p = subprocess.Popen(cmd, shell=False,
+                     stdin=subprocess.PIPE,
+                     stdout=subprocess.DEVNULL,
+                     stderr=subprocess.PIPE)
+stdoutdata, stderrdata = p.communicate()
+flag = p.returncode
+if flag != 0:
+    print(stderrdata)
+
