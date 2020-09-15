0f361990c559b7b64a55a4bcae5d2da0e4de6db8
Kavi Karan <kavipiriya.karan@gmail.com>
2020-09-14 21:10:15 -0400

    added new user (old)

--- test2.py
+++ test2.py
@@ -20,17 +20,4 @@ t2image = glob.glob(os.path.join(test_folder, '*t2*.nii.gz'))
 
 print(t2image)
 
-# # Import files into ants
-# test_data = ants.image_read(t2image[0])
-#
-# # Display images in itksnap, if installed
-# cmd = ["itksnap", "-g", t2image[0]]
-#
-# p = subprocess.Popen(cmd, shell=False,
-#                      stdin=subprocess.PIPE,
-#                      stdout=subprocess.DEVNULL,
-#                      stderr=subprocess.PIPE)
-# stdoutdata, stderrdata = p.communicate()
-# flag = p.returncode
-# if flag != 0:
-#     print(stderrdata)
+
