22eef763727e2ee80298003c06aa10baabaf437c
Kavi Karan <Kavipiriya.Karan@gmail.com>
2020-09-22 21:42:57 -0400

    tried out ants segmentation

--- test2.py
+++ test2.py
@@ -1,5 +1,5 @@
 import os
-# import ants
+import ants
 import glob
 import subprocess
 import getpass
@@ -10,6 +10,7 @@ from PyQt5.QtWidgets import QApplication, QLabel
 from dependencies import ROOTDIR, CONFIGDATA
 from utils.HelperFunctions import Output, Configuration, Imaging
 
+# initializing a QT application instance
 app = QApplication([])
 
 try:
@@ -35,7 +36,19 @@ elif os.getlogin() == 'root':
 
 t2image = glob.glob(os.path.join(test_folder, '*t2*.nii.gz'))
 
-print(t2image)
-Imaging.load_imageviewer(path2viewer=CONFIGDATA["folders"][getpass.getuser()]["path2itksnap"], file_names=t2image)
+# print(t2image)
+# Imaging.load_imageviewer(path2viewer=CONFIGDATA["folders"][getpass.getuser()]["path2itksnap"], file_names=t2image)
 
 # Start loading data to Ants
+img = ants.image_read(t2image[0])
+
+print(img)
+
+#
+img = ants.resample_image(img, (128, 128, 128), True, 0)
+mask = ants.get_mask(img, (128, 128, 128), 1, 0)
+print(img)
+#
+img_seg = ants.atropos(a=img, m='[0.2, 1x1x1]', c='[5,0]', i='Kmeans[3]', x=mask)
+print(img_seg.keys())
+ants.plot(img_seg['segmentation'])
