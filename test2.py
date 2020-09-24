17312eff3c43459e428d6953f88be6ea5ce9a7d2
dpedrosac <david.pedrosa@staff.uni-marburg.de>
2020-09-25 00:08:27 +0200

    Example for possible start of pipeline with N4Bias Correction.

--- test2.py
+++ test2.py
@@ -34,13 +34,31 @@ elif os.getlogin() == 'root':
         test_folder = os.path.join(rootdir, 'data/patients', '4839P')
         print(test_folder)
 
+
 t2image = glob.glob(os.path.join(test_folder, '*t2*.nii.gz'))
+if len(t2image) < 1:
+    Output.msg_box(text="No files were found, please make sure only one file is present",
+                   title="no files present")
+elif len(t2image) > 1:
+    Output.msg_box(text="Too many files were found, please make sure only one file is present",
+                   title="too many files in the directory")
 
 # print(t2image)
 # Imaging.load_imageviewer(path2viewer=CONFIGDATA["folders"][getpass.getuser()]["path2itksnap"], file_names=t2image)
 
+# ==============================          N4BiasCorrection          ==============================
 # Start loading data to Ants
 original_image = ants.image_read(t2image[0])
+
+# ==============================          Rescale Images to positive values          ==============================
+rescaler_nonneg = ants.contrib.RescaleIntensity(10, 100)  # to avoid values <0 causing problems w/ log data
+min_orig, max_orig = original_image.min(), original_image.max()
+
+rescale = True
+if rescale:
+    original_image_nonneg = rescaler_nonneg.transform(original_image)
+else:
+    original_image_nonneg = original_image
 
 # ==============================          N4BiasCorrection          ==============================
 # General settings
@@ -49,24 +67,15 @@ convergence = [50,50,50,50]
 threshold = 1e-07
 bspline_fitting = 200
 filename_save_bcorr = 'bcorr-' + os.path.split(t2image[0])[1]
-bcorr_image = ants.utils.n4_bias_field_correction(original_image,
+bcorr_image = ants.utils.n4_bias_field_correction(original_image_nonneg,
                                                   mask=None,
                                                   shrink_factor=shrink_factor,
                                                   convergence={'iters':convergence, 'tol': threshold},
-                                                  spline_param= bspline_fitting, verbose=True, weight_mask=None)
+                                                  spline_param=bspline_fitting, verbose=True)
 
 ants.image_write(bcorr_image, filename=filename_save_bcorr)
 
 filename_save_bcorrdiff = 'bcorr_diff-' + os.path.split(t2image[0])[1]
-diff_image = original_image - bcorr_image
+diff_image = original_image_nonneg - bcorr_image
 ants.image_write(diff_image, filename=os.path.join(os.path.split(t2image[0])[0], filename_save_bcorrdiff))
 
-
-
-#img = ants.resample_image(img, (128, 128, 128), True, 0)
-#mask = ants.get_mask(img, (128, 128, 128), 1, 0)
-#print(img)
-#
-#img_seg = ants.atropos(a=img, m='[0.2, 1x1x1]', c='[5,0]', i='Kmeans[3]', x=mask)
-#print(img_seg.keys())
-#ants.plot(img_seg['segmentation'])
