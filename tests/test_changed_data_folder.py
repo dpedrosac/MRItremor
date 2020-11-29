ef904f80e9ac5f8f447c32d8021b5e1a0e02bd73
Kavi Karan <Kavipiriya.Karan@gmail.com>
2020-11-29 06:10:22 +0100

    tested new probability mask

--- tests/test_changed_data_folder.py
+++ tests/test_changed_data_folder.py
@@ -1,8 +1,13 @@
 from dependencies import FILEDIR, ROOTDIR
 import glob
+import ants
 
 
-print(FILEDIR)
-print(ROOTDIR)
+TEMPLATE_PATH = f"{FILEDIR}/template"
 
-print(glob.glob(f"{FILEDIR}/*"))
+prob_img = glob.glob(f"{FILEDIR}/template/*.nii")[0]
+
+prob_mask = ants.image_read(prob_img)
+mask = ants.threshold_image(prob_mask, 0.5, 1, 1, 0)
+ants.plot(prob_img, mask)
+ants.image_write(mask, f"{TEMPLATE_PATH}/mask.nii.gz")
