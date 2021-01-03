45b019d96d63be65293e4d1997754b5c9e8e5b96
Kavi Karan <Kavipiriya.Karan@gmail.com>
2021-01-03 16:57:23 +0100

    added new seg code for creating seg from all preprocessed images.

--- segmentation.py
+++ segmentation.py
@@ -8,44 +8,49 @@ from utils.HelperFunctions import Configuration, Output, FileOperations, Imaging
 from dependencies import FILEDIR, ROOTDIR
 
 
-def get_registered_image(patient_id='4698P', type='t2'):
-    patient_folder = os.path.join(ROOTDIR, FILEDIR, patient_id, 'output')
-    file = glob.glob(f"{patient_folder}/wfi*{type}*")
-    return file[0]
+def get_preprocessed_image():
+    patient_folder = os.path.join(FILEDIR, 'preprocessed')
+    files = glob.glob(f"{patient_folder}/*")
+    return files
 
 
-# TEST_PATIENT = '4698P'
-TEST_PATIENT = '4701P'
+OUTPUT_FOLDER = os.path.join(FILEDIR, 'seg_output')
 
 
-start_time = time.time()
-print("Starting...")
+def do_seg(image_path):
+    start_time = time.time()
+    print("Starting...")
+    # print(image_path)
 
-image_path = get_registered_image(patient_id=TEST_PATIENT)
+    unique_id = image_path.split('_')[3]
 
-img = ants.image_read(image_path)
-mask = ants.get_mask(img)
+    img = ants.image_read(image_path)
+    mask = ants.get_mask(img)
 
-print("Loading images completed in %2.f seconds" % (time.time() - start_time))
+    print("Loading images completed in %2.f seconds" % (time.time() - start_time))
 
-start_time = time.time()
-print("Starting segmentation...")
+    start_time = time.time()
+    print("Starting segmentation...")
 
-segs = ants.atropos(a=img, x=mask, c='[2,0]', m='[0.2, 1x1x1]', i='kmeans[3]')
+    segs = ants.atropos(a=img, x=mask, c='[2,0]', m='[0.2, 1x1x1]', i='kmeans[3]')
 
-print("Segmentation done in", "%.2f seconds" % (time.time() - start_time))
+    print("Segmentation done in", "%.2f seconds" % (time.time() - start_time))
 
-output_folder = os.path.join(ROOTDIR, FILEDIR, TEST_PATIENT, 'seg_output')
-ants.image_write(segs['segmentation'], f"{output_folder}/seg_atropos_{TEST_PATIENT}.nii")
+    output_folder = os.path.join(OUTPUT_FOLDER)
+    ants.image_write(segs['segmentation'], f"{output_folder}/seg_atropos_{unique_id}.nii")
 
-start_time = time.time()
-print("Starting cortical thickness calculation")
+    start_time = time.time()
+    print("Starting cortical thickness calculation")
 
-thickimg = ants.kelly_kapowski(s=segs['segmentation'],
-                               g=segs['probabilityimages'][1],
-                               w=segs['probabilityimages'][2],
-                               its=45, r=0.5, m=1)
+    thickimg = ants.kelly_kapowski(s=segs['segmentation'],
+                                   g=segs['probabilityimages'][1],
+                                   w=segs['probabilityimages'][2],
+                                   its=45, r=0.5, m=1)
 
-print("Calculation done in", "%.2f seconds" % (time.time() - start_time))
+    print("Calculation done in", "%.2f seconds" % (time.time() - start_time))
 
-ants.image_write(thickimg, f"{output_folder}/seg_thickimg_{TEST_PATIENT}.nii")
+    ants.image_write(thickimg, f"{output_folder}/seg_thickimg_{unique_id}.nii")
+
+
+for image in get_preprocessed_image():
+    do_seg(image)
