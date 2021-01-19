609b759a78580c76ce5ad8190b732f2c3b39e7bc
dpedrosac <david.pedrosa@staff.uni-marburg.de>
2021-01-20 00:26:50 +0100

    Further improvements

--- utils/AtroposSegmentation.py
+++ utils/AtroposSegmentation.py
@@ -71,39 +71,35 @@ def segmentationAtropos(imaging, template_sequence, fileID, c='[2,0]', m='[0.2, 
     template_brain_mask = template_mask * template_image
 
     for idx, image in enumerate(imaging):
-        print("\n{}\nProcessing image {} ( out of {} )\n{}".format('=' * 85, idx + 1, len(imaging), '=' * 85))
+        filename2save = 'kk' + fileID[idx] + '.nii.gz'
+        if not os.path.isfile(os.path.join(output_folder, filename2save)):
+            print("\n{}\nProcessing image {} ({} remaining )\n{}".format('=' * 85, fileID[idx], len(imaging)-idx, '=' * 85))
 
-        # =================     Atropos segmentation wth given priors     =================
-        image = ants.image_read(os.path.join(preprocessed_folder, image)) if isinstance(image, str) else image
-        if getpass.getuser() == 'david':
-            mask = ants.image_read(os.path.join(masks_folder, 'mask' + fileID[idx] + '.nii.gz'))
-            # unnecessary except for david's machine
-        else:
-            mask = ants.get_mask(image)  # replace this part with ants.read_image
+            # =================     Atropos segmentation wth given priors     =================
+            image = ants.image_read(os.path.join(preprocessed_folder, image)) if isinstance(image, str) else image
+            if getpass.getuser() == 'david':
+                mask = ants.image_read(os.path.join(masks_folder, 'mask' + fileID[idx] + '.nii.gz'))
+                # unnecessary except for david's machine
+            else:
+                mask = ants.get_mask(image)  # replace this part with ants.read_image
 
-        if PRIOR_TEMPLATE:
-            segs = ants.atropos(a=image, x=mask, c=c, m=m, i=i) #,
-                                # p=os.path.join(ROOTDIR, 'data', 'cookedPriors',
-                                #                 'prior%01d.nii.gz'), v=1)
-            # priors = 'priorProbabilityImages[6,{},0.5]'.format(os.path.join(ROOTDIR, 'data',
-            #                                                                'cookedPriors',
-            #                                                                'prior%01d.nii.gz'))
-            # segs2 = ants.atropos(a=image, x=mask, c=c, m=m, i=priors, p='Socrates[0]', v=1)
-        else:
-            segs = ants.atropos(a=image, x=mask, c=c, m=m, i=i)
+            if PRIOR_TEMPLATE:
+                segs = ants.atropos(a=image, x=mask, c=c, m=m, i=i,
+                                    p=os.path.join(ROOTDIR, 'data', 'cookedPriors',
+                                                     'prior%01d.nii.gz'), v=1)
+                # priors = 'priorProbabilityImages[6,{},0.5]'.format(os.path.join(ROOTDIR, 'data',
+                #                                                                'cookedPriors',
+                #                                                                'prior%01d.nii.gz'))
+                # segs2 = ants.atropos(a=image, x=mask, c=c, m=m, i=priors, p='Socrates[0]', v=1)
+            else:
+                segs = ants.atropos(a=image, x=mask, c=c, m=m, i=i)
 
-        kk_segmentation = segs['segmentation']
-        kk_segmentation[kk_segmentation == 4] = 3
-        kk_gray_matter = segs['probabilityimages'][2]
-        kk_white_matter = segs['probabilityimages'][3] + segs['probabilityimages'][4]
-        kk = ants.kelly_kapowski(s=kk_segmentation, g=kk_gray_matter, w=kk_white_matter, its=45, r=0.025, m=1.5, x=0,
-                                 verbose=1)
+            kk_segmentation = segs['segmentation']
+            kk_segmentation[kk_segmentation == 4] = 3
+            kk_gray_matter = segs['probabilityimages'][2]
+            kk_white_matter = segs['probabilityimages'][3] + segs['probabilityimages'][4]
+            kk = ants.kelly_kapowski(s=kk_segmentation, g=kk_gray_matter, w=kk_white_matter, its=45, r=0.025, m=1.5, x=0,
+                                     verbose=1)
 
-
-        thickimg = ants.kelly_kapowski(s=segs['segmentation'],
-                                       g=segs['probabilityimages'][1],
-                                       w=segs['probabilityimages'][2],
-                                       its=45, r=0.5, m=1)
-
-        filename2save = 'xxx' + '' + 'WarpedToTemplate.nii.gz'
-        ants.image_write(segs, os.path.join(output_folder, filename2save))
+            filename2save = 'kk' + fileID[idx] + '.nii.gz'
+            ants.image_write(kk, os.path.join(output_folder, filename2save))
