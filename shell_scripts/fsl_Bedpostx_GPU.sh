3330348ae748d9c73a2a0ef7e4f039aa1d297f12
david <46906261+dpedrosac@users.noreply.github.com>
2021-05-25 21:32:12 +0200

    Minor changes in the code

--- shell_scripts/fsl_Bedpostx_GPU.sh
+++ shell_scripts/fsl_Bedpostx_GPU.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
 # author: Irina Palaghia
-# version: 2021-04-29, mod by DP 2021-04-30
+# version: 2021-04-29, mod by DP 2021-04-30 and 2021-05-25
 # this script runs the bedpostX routines with GPU support for GTX3090
 # for this purpose, FSL should be installed as described here:
-# https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/FslInstallation and additional drivers are required (cf. )
-
+# https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/FslInstallation
 
 CURRENT_DIR=${PWD}
 PARAMETER_DIR=${PWD}/bash/ # all settings for eddy are stored hree
 
-function_prepare_data()
-{
-	# cd $1 # necessary?!
+function_prepare_data(){
     OUTPUT_DIR=$1/bedpostX # creates output folder 
     if [[ ! -d $OUTPUT_DIR ]];
     then
-		mkdir -p $OUTPUT_DIR
-		chmod -R 777 ${OUTPUT_DIR}
-     fi
+	mkdir -p $OUTPUT_DIR
+	chmod -R 777 ${OUTPUT_DIR}
+    fi
 
 	cp $2/rawdata/ep2d_diff_rolled.bvals ${OUTPUT_DIR}
 	cp $1/eddycorrected.eddy_rotated_bvecs ${OUTPUT_DIR}
 	cp $1/eddycorrected.nii.gz ${OUTPUT_DIR}
 	cp $1/bet_brain_mask.nii.gz ${OUTPUT_DIR}
 
-	# renames all data according to the nomenclature needed  to run the script
+	# renames data according to the nomenclature needed to run script
 	mv ${OUTPUT_DIR}/ep2d_diff_rolled.bvals ${OUTPUT_DIR}/bvals.txt
 	mv ${OUTPUT_DIR}/eddycorrected.eddy_rotated_bvecs ${OUTPUT_DIR}/bvecs.txt
 	mv ${OUTPUT_DIR}/eddycorrected.nii.gz ${OUTPUT_DIR}/data.nii.gz
@@ -38,7 +35,7 @@ for WORKING_DIR in ${PWD}/preprocessed/*/; do # runs eddy for every preprocessed
         echo "Running bedpostX with GPU support at $WORKING_DIR"
         echo
 	function_prepare_data ${WORKING_DIR} ${CURRENT_DIR}
-	# ${FSLDIR}/src/fdt/CUDA/bedpostx_gpu ${WORKING_DIR}/bedpostX/
+	${FSLDIR}/src/fdt/CUDA/bedpostx_gpu ${WORKING_DIR}/bedpostX/
 	
 done
 echo "          ...done with bedpostX (FSL) for all subjects. Please perform visual checks"
